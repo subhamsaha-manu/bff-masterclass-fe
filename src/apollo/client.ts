4ec404867afab400b0169d0b0010e18ebff6a65c
Subham Saha <subhamsaha90@gmail.com>
2023-09-13 01:07:35 +0530

    Initial commit

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,36 +1,13 @@
-import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache, split } from '@apollo/client'
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
-import { getMainDefinition } from '@apollo/client/utilities'
-import { createClient } from 'graphql-ws'
+import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache } from '@apollo/client'
 
 import { USER_TOKEN } from '@/utils/constants'
 import { storage } from '@/utils/storage'
 
 console.log(`NODE_ENV set to: `, process.env.NODE_ENV)
-const isProduction = process.env.NODE_ENV === 'production'
-//const isProduction = false
 
 const httpLink = createHttpLink({
-  uri: isProduction
-    ? 'https://birthday-reminder-backend.herokuapp.com/graphql'
-    : 'http://localhost:9006/graphql',
+  uri: 'http://localhost:9006/graphql',
 })
-
-const wsLink = new GraphQLWsLink(
-  createClient({
-    url: 'ws://localhost:4000/subscriptions',
-  })
-)
-
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query)
-
-    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-  },
-  wsLink,
-  httpLink
-)
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   // add the authorization to the headers
@@ -45,7 +22,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 
 export const getClient = () => {
   return new ApolloClient({
-    link: ApolloLink.from([authMiddleware, splitLink]),
+    link: ApolloLink.from([authMiddleware, httpLink]),
     cache: new InMemoryCache(),
   })
 }
